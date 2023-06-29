3e3c00c22dad67d7e459d4a9b1d9cfa1022f8329
Nicoletta <Nicoletta@192.168.1.7>
2023-06-29 12:15:47 +0200

    Soluzione_1_GetBestNodo

--- src/main/java/it/polito/tdp/yelp/db/YelpDao.java
+++ src/main/java/it/polito/tdp/yelp/db/YelpDao.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.polito.tdp.yelp.model.Archi;
 import it.polito.tdp.yelp.model.Business;
 import it.polito.tdp.yelp.model.Review;
 import it.polito.tdp.yelp.model.User;
@@ -110,6 +111,113 @@ public class YelpDao {
 			return null;
 		}
 	}
+	//Primo punto:
+	public List<String> getAllCities(){
+		String sql = "SELECT DISTINCT city "
+				+ "FROM business "
+				+ "ORDER BY city";
+		List<String> result = new ArrayList<String>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+
+				String city = res.getString("city");
+				result.add(city);
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
+	//Secondo punto:
+	public List<Business> getAllBusinesswithcondition(int anno, String citta){
+		String sql = "SELECT * "
+				+ "FROM business "
+				+ "WHERE city = ? "
+				+ "AND ( "
+				+ "   SELECT COUNT(*) "
+				+ "	  FROM reviews "
+				+ "   WHERE business.business_id = reviews.business_id "
+				+ "   AND YEAR(review_date) = ? "
+				+ ") > 0 "
+				+"ORDER BY business_name ASC";
+		List<Business> result = new ArrayList<Business>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(2, anno);
+			st.setString(1, citta);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Business business = new Business(res.getString("business_id"), 
+						res.getString("full_address"),
+						res.getString("active"),
+						res.getString("categories"),
+						citta,
+						res.getInt("review_count"),
+						res.getString("business_name"),
+						res.getString("neighborhoods"),
+						res.getDouble("latitude"),
+						res.getDouble("longitude"),
+						res.getString("state"),
+						res.getDouble("stars"));
+				result.add(business);
+				
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
+	//Terzo punto; I cammini del grafo: (Utilizzo la classe Archi)
+	public List<Archi> getAllArchi(int anno,String citta){
+		String sql = "SELECT b1.business_id AS idB1, b2.business_id AS idB2, AVG(r2.stars)-AVG(r1.stars) AS differenza "
+				+ "FROM reviews r1, reviews r2, business b1, business b2 "
+				+ "WHERE r1.business_id=b1.business_id AND r2.business_id=b2.business_id AND b1.business_id <> b2.business_id AND YEAR(r1.review_date)=YEAR(r2.review_date) AND YEAR(r1.review_date)=? AND b1.city=b2.city AND b1.city=? "
+				+ "GROUP BY b1.business_id, b2.business_id "
+				+ "HAVING differenza>0";
+		List<Archi> result = new ArrayList<Archi>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, anno);
+			st.setString(2, citta);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Archi a = new Archi(res.getString("idB1"),res.getString("idB2"),res.getDouble("differenza"));
+				result.add(a);
+			}
+			res.close();
+			st.close();
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	/**
+	 * SELECT distinct b.* "
+				+ "FROM business b, reviews r "
+				+ "WHERE YEAR(r.review_date) = ? AND b.city = ? AND b.business_id=r.business_id
+	 */
 }
--- src/main/java/it/polito/tdp/yelp/model/Model.java
+++ src/main/java/it/polito/tdp/yelp/model/Model.java
@@ -1,6 +1,81 @@
 package it.polito.tdp.yelp.model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleDirectedWeightedGraph;
+
+import it.polito.tdp.yelp.db.YelpDao;
+
 public class Model {
 	
 	
+	Graph<Business, DefaultWeightedEdge> grafo;
+	List<Business> vertici;
+	Map<String,Business> idMap;
+	
+	public List<String> getAllCities() {
+		YelpDao dao = new YelpDao();
+		return dao.getAllCities();
+	}
+	
+	public void creaGrafo(int anno,String citta) {
+		//Secondo punto:creare il grafo
+		grafo = new SimpleDirectedWeightedGraph<Business, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		YelpDao dao = new YelpDao();
+		//Aggiungo i vertici al grafo:
+		vertici = dao.getAllBusinesswithcondition(anno, citta);
+		idMap= new HashMap<String,Business>();
+		for(Business b : vertici) {
+			idMap.put(b.getBusinessId(), b);
+		}
+		Graphs.addAllVertices(grafo, vertici);
+		
+		//Terzo punto: Calcolare i cammini del grafo:
+		List<Archi> archi = dao.getAllArchi(anno, citta);
+		for(Archi a : archi) {
+			
+			Graphs.addEdge(grafo, idMap.get(a.getBusiness_id_1()), idMap.get(a.getBusiness_id_2()),a.getPeso());
+			
+		}
+	}
+	
+	public int getVertex() {
+		return grafo.vertexSet().size();
+	}
+	
+	public int getEdges() {
+		return grafo.edgeSet().size();
+	}
+	
+	public Business getBestB() {
+		
+		double max_peso = 0.0;
+		Business result = null;
+		
+		for(Business b : grafo.vertexSet()) {
+			double val = 0.0;
+			
+			for(DefaultWeightedEdge e : grafo.incomingEdgesOf(b)) {
+				val += grafo.getEdgeWeight(e);
+			}
+			for(DefaultWeightedEdge e : grafo.outgoingEdgesOf(b)) {
+				val -= grafo.getEdgeWeight(e);
+			}
+			
+			if(val > max_peso) {
+				max_peso = val;
+				result = b;
+			}
+		}
+		
+		
+		return result;
+	}
 }
